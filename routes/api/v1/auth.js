7b5255e1d5a2dde6aa3091e20b5954926d82fe6c
Kashi Samaraweera <kashi@kashmaniac.com.au>
2015-08-07 09:40:58 +1000

    Working on mysql integration.

--- routes/api/v1/auth.js
+++ routes/api/v1/auth.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
 
 /**
  * POST to tokens
  */
 router
-    .post(/tokens(\.xml|\.json)?/i, function(req, res, next) {
-        var apiTarget = req.apiTarget;
+    .post('/tokens(.xml|.json)?', function(req, res, next) {
+        var apiTarget = req.apiTarget,
+            dbConfig = require('../../../auth/modules/auth/config.json').database;
+        //res.json(dbConfig);
         res.send("Your new authentication token in " + apiTarget.responseFormat + " form.");
     })
     .get('/tokens(.xml|.json)?/:token?', function(req, res, next) {
@@ -18,6 +21,23 @@ router
     .delete('/tokens(.xml|.json)?/:token', function(req, res, next) {
         res.send("Invalidating YOUR TOKEN");
     });
+
+/**
+ * Establishes a connection to the database, returning the connection object
+ * ready for query running.
+ * @param {string} authFile The file with authentication credentials for the
+ *                          MySQL database being used.
+ * @returns {*} Returns a MySQL connection object that may be used to run
+ *              queries against.
+ */
+function connect_mysql(authFile) {
+    var authFileContents,
+        connection;
+
+    //mysql.createConnection();
+
+    return {};
+}
 
 
 module.exports = router;
